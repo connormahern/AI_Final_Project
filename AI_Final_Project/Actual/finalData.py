4ab4a58bf87105fa5bac1a9fdc0978dfd640b2fc
connormahern <54722511+connormahern@users.noreply.github.com>
2020-05-01 23:13:12 -0400

    Add files via upload

---
+++ AI_Final_Project/Actual/finalData.py
@@ -0,0 +1,143 @@
+
+path = '/Users/connormahern/Desktop/AI_Final_Project/'
+
+censusData = path + 'AI_FinalProject/censusData.csv'
+ageSexData = path + 'AI_FinalProject/ageSexData.csv'
+mobilityData = path + 'AI_FinalProject/google_mobility_data.csv'
+popDensityData = path + 'AI_FinalProject/popDensityState.csv'
+sexDistData = path + 'AI_FinalProject/output.csv'
+
+import json
+import urllib.request
+import math
+import pandas as pd
+import heapq
+import numpy as np
+import matplotlib.pyplot as plt
+from geopy.geocoders import Nominatim
+from sklearn.linear_model import LinearRegression
+
+
+
+url = "https://coronavirus.m.pipedream.net/"
+
+# open json
+with urllib.request.urlopen(url) as url:
+    data = json.loads(url.read().decode())
+
+# city object
+class City():
+
+    def __init__(self, place, state, country, cases, deaths, recovered, lat, long, unique):
+        self.place = place
+        self.state = state
+        self.country = country
+        self.cases = cases
+        self.deaths = deaths
+        self.recovered = recovered
+        self.lat = lat
+        self.long = long
+        self.unique = unique
+
+        if 0 <= int(self.cases) <= 100:
+            self.severity = ["fcb900", 'low']
+        elif 101 <= int(self.cases) <= 1000:
+            self.severity = ["fc6100", 'medium']
+        elif int(self.cases) > 1000:
+            self.severity = ["ff0d00", 'high']
+
+
+# put all data into this
+caseInfo = data['rawData']
+
+# init locations countries, and states
+locations = []
+
+# loop through data
+for i in range(len(caseInfo)):
+
+    # get current place
+    place = caseInfo[i]
+
+    # if there are any cases
+    if int(place['Confirmed']) != 0:
+        # create object  place, state, country, cases, deaths, recovered, lat, long, unique
+        locations.append(
+            City(place['Combined_Key'], place['Province_State'], place['Country_Region'], place['Confirmed'],
+                  place['Deaths'], place['Recovered'], place['Lat'], place['Long_'], place['FIPS']))
+
+# sort list by number of confirmed cases
+locations.sort(key=lambda x: x.place, reverse=False)
+
+"""## Find nearest city to user in corona dataset"""
+
+def nearest(pointA):
+    # function to calc distance
+    def calc_euclidean_distance(point_a, point_b):
+        return math.sqrt(math.pow(point_a[0] - point_b[0], 2) + math.pow(point_a[1] - point_b[1], 2))
+
+    # init heap
+    heap = []
+
+    # loop through all locations to find closest
+    for index, place in enumerate(locations):
+        # if if its not empty
+        if (place.long != "") and (place.lat != ""):
+
+            long = float(place.long)
+            lat = float(place.lat)
+
+            pointB = [long, lat]
+            d = [calc_euclidean_distance(pointA, pointB), index]
+
+            heapq.heappush(heap, d)
+
+    nearest = heapq.heappop(heap)[1]
+
+    return(locations[nearest])
+
+"""## Calc Probabilities"""
+
+def prob(cityObj):
+
+    # get string name
+    strngName = cityObj.place.split(", ")
+
+    # read sex by county and returns a male to female distribution
+    dfSDist = pd.read_csv(sexDistData)
+    df4 = dfSDist[dfSDist['STNAME'].str.contains(strngName[1]) & dfSDist['CTYNAME'].str.contains(strngName[0])]
+
+    #Adding all columns
+    tot_pop = df4['TOT_POP']
+    tot_pop = tot_pop[tot_pop.idxmax()]
+    
+    #MEAN DISTRIBUTION OF AGE BY COUNTY
+    age_groups = ['Total', '0:4', '5:9',  '10:14', '15:19',  '20:24', '25:29', '30:34', '35:39', '40:44', '45:49', '50:54', '55:59', '60:64', '65:69', '70:74', '75:79', '80:84', '85+']
+    age_tot = []
+
+    for row in df4['TOT_POP'] :
+        age_tot.append(row)
+
+    tot_pop = age_tot[0]
+    age_groups = age_groups[1:]
+    age_tot = age_tot[1:]
+
+    #CASES PER AGE RANGE
+    casesT = cityObj.cases
+    casesP = int(casesT) / tot_pop
+    ageG_cases = []
+
+    for row in age_tot :
+        ageG_cases.append(math.floor(row * casesP))
+
+
+    return {'CTNY' : cityObj.place, 'STN' : cityObj.state, 'TOT POP': tot_pop, 'TOT CASES': cityObj.cases, 'AGE RANGE' : age_groups, 'TPOP PA PCN' : age_tot, 'PRECENT OF POP INFECTED' : casesP, 'Number of Cases Per Age Range' : ageG_cases}
+
+
+
+    
+
+
+
+
+
---
+++ AI_Final_Project/Actual/finalModeling.py
@@ -0,0 +1,159 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from geopy.geocoders import Nominatim
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#IMPORTING finalDATA.py 
+import finalData as fD
+
+
+# DATA COLLECTING
+location = None
+while not location:
+  # userLoc = input()
+  userLoc = 'Chicago, IL'
+  geolocator = Nominatim(user_agent="test1")
+  location = geolocator.geocode(userLoc)
+  if not location:
+    print('invalid location, pls try again')
+
+# userAge = input()
+userAge = 21
+
+# get closest county to my coords
+closestCtyOb = fD.nearest((location.longitude, location.latitude))
+
+# print prob of getting the infected in the county
+city1 = fD.prob(closestCtyOb)
+
+
+def countyFrame(CNTY) :
+    data = {
+        'CNTY NAME' : CNTY['CTNY'],
+        'AGE RANGE' : CNTY['AGE RANGE'],
+        'TOT POP BY AGE GROUP' : CNTY['TPOP PA PCN'],
+        'CASES BY AGE GROUP' : CNTY['Number of Cases Per Age Range']
+    }
+
+    df = pd.DataFrame(data)
+    df['PRECENT CASES BY AGE GROUP'] = (df['CASES BY AGE GROUP'] / df['TOT POP BY AGE GROUP'])*100
+
+    #OUTBREAK THRESHOLD - can be described as 1 / Log(R0) -- R0 is the Basic Repoduction number, i.e. the number of people infected by one case
+    #R0 number as of April 7, 2020 reported as 2.2 - 2.7 (2.4)
+    outbreak_threshold = 1 / (math.log(5.7))
+    #print(outbreak_threshold)
+
+
+
+    df['IsOutbreak'] = df['PRECENT CASES BY AGE GROUP'] >= outbreak_threshold
+
+    return df
+
+
+df1 = countyFrame(city1)
+
+#BUIDLING TESTING DATA SET
+
+
+path = '/Users/connormahern/Desktop/AI_Final_Project/'
+sexDistData = path + 'AI_FinalProject/uscities.csv'
+
+#Single City Test Cases
+'''
+city = 'Evansville, IN'
+
+geolocator1 = Nominatim(user_agent="test1")
+location1 = geolocator1.geocode(city)
+closestCtyOb = fD.nearest((location1.longitude, location1.latitude))
+
+cityProb = fD.prob(closestCtyOb)
+cityProb = countyFrame(cityProb)
+
+print(cityProb)
+
+
+'''
+dfCities = pd.read_csv(sexDistData, usecols = ['city','state_id'])
+
+dfCities = dfCities.sample(n=25)
+
+frames = []
+
+for elm in dfCities.iterrows():
+
+    try :
+        city = elm[1][0] + ', ' + elm[1][1]
+        print(city)
+        
+        geolocator1 = Nominatim(user_agent="test1")
+        location1 = geolocator1.geocode(city)
+        closestCtyOb = fD.nearest((location1.longitude, location1.latitude))
+        #print(closestCtyOb)
+
+        cityProb = fD.prob(closestCtyOb)
+        cityProb = countyFrame(cityProb)
+
+        frames.append(cityProb)
+        index += 1
+
+    except :
+        continue
+
+df2 = pd.concat(frames)
+
+ids = df2[['CNTY NAME', 'AGE RANGE']]
+y = df2['IsOutbreak']
+X = df2.drop(['CNTY NAME', 'AGE RANGE', 'IsOutbreak'], axis=1)
+print(X)
+print(y)
+
+
+
+#modeling
+
+model = LogisticRegression(random_state=42)
+model.fit(X, y)
+
+y_hat = pd.Series(model.predict(X), name='Pred IsOutbreak')
+
+
+print(accuracy_score(y, y_hat))
+
+#checking the model with R^2 - 
+
+#print(len(ids))
+#print(len(y))
+#print(len(y_hat))
+
+
+output_df = pd.concat([ids, y, y_hat], axis=0)
+#print(output_df.head())
+
+
+
+outbreak_prob = pd.DataFrame(model.predict_proba(X), columns=['Prob Not Outbreak', 'Pred Prob to be Outbreak']) 
+#print(outbreak_prob.head())
+
+
+#df2 = df2.drop(['PRECENT CASES BY AGE GROUP', 'IsOutbreak', 'TOT POP BY AGE GROUP', 'CASES BY AGE GROUP'],  axis = 1)
+#outbreak_prob = outbreak_prob.drop(['Prob Not Outbreak'], axis = 1)
+
+df2.reset_index(drop=True, inplace=True)
+outbreak_prob.reset_index(drop=True, inplace=True)
+
+print(df2.head)
+print(outbreak_prob)
+
+# result = pd.concat([df2, outbreak_prob], axis=1, sort=False)
+result = df2.join(outbreak_prob)
+print(result)
+
+
+
+
+
+
+
